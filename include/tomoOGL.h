9b56cb4eaa2e6502e2b04c0601697d923bd938fc
Maxim Doronin <doro156@me.com>
2015-11-28 00:00:40 +0300

    Add drawing class uses OpenGL: TomoOGL

---
+++ include/tomoOGL.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QGLWidget>
+#include "tomo_data.h"
+
+class TomoOGL : public QGLWidget
+{
+	Q_OBJECT
+
+public:
+	TomoOGL(char* file, QWidget *parent = 0);
+	~TomoOGL(){};
+	void upd(int _lay);
+
+private:
+	void initializeGL();
+	void resizeGL(int width, int height);
+	void paintGL();
+
+	int lay;		//отображаемый слой
+	int flag;
+	Tomo_Data *t;
+
+};
---
+++ src/tomoOGL.cpp
@@ -0,0 +1,49 @@
+#include "tomoOGL.h"
+
+TomoOGL::TomoOGL(char* file, QWidget *parent)
+	: QGLWidget(parent)
+{
+	t = new Tomo_Data(file);
+	lay = 0;
+	flag = 0;
+}
+
+void TomoOGL::initializeGL()
+{
+	qglClearColor(Qt::black);
+	glEnable(GL_CULL_FACE);
+}
+
+void TomoOGL::resizeGL(int width, int height)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	glOrtho(0.0, t->data_size.x, 0.0, t->data_size.y, -1.0, 1.0);
+	glViewport(0, 0, (GLint)width, (GLint)height);
+}
+
+void TomoOGL::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW); 
+    glLoadIdentity();           
+
+	if (flag) t->pixels_delete();
+	t->get_lay(lay);
+	glBegin(GL_POINTS);
+		for (int i = 0; i < t->data_size.x; i++)	
+			for (int j = 0; j < t->data_size.y; j++)
+			{
+				glColor3ub(t->data_pixels[i * t->data_size.y + j], t->data_pixels[i * t->data_size.y + j], t->data_pixels[i * t->data_size.y + j]);
+				glVertex2i(i, j);
+			}
+	glEnd();
+	flag = 1;
+}
+
+void TomoOGL::upd(int _lay)
+{
+	this->lay = _lay;
+	updateGL();
+}
